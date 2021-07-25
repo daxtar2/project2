7229f8a841de64c6282f38d2c166710f45d6f97b
daxtar2 <87515461+daxtar2@users.noreply.github.com>
2021-07-25 22:08:38 +0800

    Add files via upload

---
+++ demo.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(void)
+{
+	int number = 50;
+	int Number = 40;
+	//  标识符
+	//50--->number()
+	//注意区分大小写,c语言分大小写
+	//有效标识符：eg A a _
+	double money = 99.9;
+	printf("number = %u\n", number);
+	printf("Number = %u\n", Number);
+	printf("money = %lf\n", money);
+	//      输出内容        变量
+	return 0;
+//‘;’表示这句话说完
+}
